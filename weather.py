e55d686753ab71b9e3b4d0136fb0ec4cdd3919e8
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-08-24 13:19:00 -0500

    Added layout for info column

--- weather.py
+++ weather.py
@@ -1,6 +1,16 @@
 import PySimpleGUI as sg
 
-layout = [[]]
+image_col = sg.Column()
+info_col = sg.Column([
+    [sg.Text("", key = "-LOCATION-", font = "Calibri 30", background_color = "#FF0000", pad = 0, visible = False)],
+    [sg.Text("", key = "-TIME-", font = "Calibri 16", background_color = "#000000", text_color = "#FFFFFF", pad = 0, visible = False)],
+    [sg.Text("", key = "-TEMP-", font = "Calibri 16", background_color = "#FFFFFF", text_color = "#000000", pad = {0, 10}, justification = "center", visible = False)]
+    ])
+
+layout = [
+    [sg.Input(expand_x = True, key = "-INPUT-"), sg.Button("Submit")],
+    [image_col, info_col]
+    ]
 
 window = sg.Window("Weather", layout)
 
